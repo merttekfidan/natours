f7eb66e280c84e6e1a48a450ef7c2a495a7e35d3
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-02-07 22:05:29 +0100

    Environment variables

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const userRouter = require('./routes/userRoutes.js');
 ////////////////////
 ////MIDDLEWARE
 app.use(express.json()); //middleware
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
   console.log('Hello from the middleware!');
   next();
