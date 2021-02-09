cca9f9b13bd269879ff46f18fbc6f678df5f5699
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-02-09 19:07:58 +0100

    Connect mongodb cluster

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  });
 console.log(process.env.NODE_ENV);
 
 const port = process.env.PORT || 3000;
