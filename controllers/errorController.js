2b71333b19cede9f19a46a1153d696390a067b8b
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-03-04 19:55:38 +0100

    Handling invalid database IDs

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -26,14 +33,18 @@ const sendErrorProd = (err, res) => {
 };
 
 module.exports = (err, req, res, next) => {
-  console.log(err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    //Interestingly, you have to define ''name'' property seperately. Otherwise, object destructering will not catch err.name
+    let error = { ...err, name: err.name };
+    if (error.name === 'CastError') {
+      error = handleCastErrorDB(error);
+      sendErrorProd(error, res);
+    }
   }
 
   res.status(err.statusCode).json({
