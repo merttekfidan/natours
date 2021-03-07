2390d0a9a7e0d094d3269c05a18a4a5120f0b08e
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-03-08 00:48:32 +0100

    Handle unhandled rejections

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ mongoose
 console.log(process.env.NODE_ENV);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting down!');
+  server.close(() => {
+    process.exit(1);
+  });
 });
