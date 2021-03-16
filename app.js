1f50bed4b9599ab2e1e8f3b32f6fc7607d2af789
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-03-16 20:54:42 +0100

    Implement request limit

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-const morgan = require('morgan');
 const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const app = express();
 const AppError = require('./utils/appError');
 const glabalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes.js');
 const userRouter = require('./routes/userRoutes.js');
+
 ////////////////////
 ////MIDDLEWARE
 app.use(express.json()); //middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  // 100 request from same ip in an hour
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
