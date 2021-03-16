2ae26657dad387eb368179eb362f83c88251584d
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-03-16 20:47:36 +0100

    Implement cookie function

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,18 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+    ),
+    // Can't be modified or accessed
+    httpOnly: true,
+  };
+  // In production server must have HTTPS connection
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+  // Remove the password from output
+  user.password = undefined;
   res.status(statusCode).json({
     status: 'success',
     token,
