ece00ed40ebfc5e05c4337e6e0a6ca1321d497e2
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-03-22 22:54:18 +0100

    Implement reviewModel with parent referencing

---
+++ models/reviewModel.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+const validator = require('validator');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review cannot be empty!'],
+    },
+    rating: {
+      type: Number,
+      max: [5, 'Rating must be below 5!'],
+      min: [1, 'Rating must be bigger than 0.'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    //PARENT REFERENCING
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour'],
+    },
+    users: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user'],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+reviewSchema.pre('save', function (next) {
+  this.populate('');
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
