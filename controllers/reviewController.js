07c6fd22c101af5db8a99dcda62f3496bd50bb01
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-03-30 21:45:30 +0200

    Add a nested get endpoint

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -4,7 +4,10 @@ const catchAsync = require('./../utils/catchAsync');
 const AppError = require('../utils/appError');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const reviews = await Review.find();
+  //Nested yaptığımızdan dolayı eğer eğer tourId parametresi doluysa reviewsları o idye göre filtreliyor
+  let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+  const reviews = await Review.find(filter);
   res.status(200).json({
     status: 'success',
     results: reviews.length,
@@ -16,7 +19,6 @@ exports.createReview = catchAsync(async (req, res, next) => {
   // Allow nested routes
   if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user.id;
-  console.log(req.body);
   const newReview = await Review.create(req.body);
   res.status(201).json({
     status: 'success',
