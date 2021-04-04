7d2474002d10b5479de6306f17699a07d6166c5a
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-04-04 22:28:17 +0200

    Add missing authentication and authorization

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,20 +2,6 @@ const express = require('express');
 
 const authController = require('./../controllers/authController');
 const userController = require('./../controllers/userController');
-
-const router = express.Router();
-
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
-router.post('/forgotPassword', authController.forgotPassword);
-router.patch('/resetPassword/:token', authController.resetPassword);
-
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword
-);
-
 const {
   getAllUsers,
   getUser,
@@ -27,9 +13,24 @@ const {
   getMe,
 } = require('./../controllers/userController');
 
-router.get('/me', authController.protect, getMe, getUser);
-router.patch('/updateMe', authController.protect, updateMe);
-router.delete('/deleteMe', authController.protect, deleteMe);
+const router = express.Router();
+
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+// Below this requires auth.
+//After this router.use all router will require authController.protect
+router.use(authController.protect);
+
+router.patch('/updateMyPassword', authController.updatePassword);
+router.get('/me', getMe, getUser);
+router.patch('/updateMe', updateMe);
+router.delete('/deleteMe', deleteMe);
+
+//After this middleware all routes below will be restricted to role mentioned in brackets (admin)
+router.use(authController.restrictTo('admin'));
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
