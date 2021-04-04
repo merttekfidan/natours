fc95d21e2b09e76806d4f5bb7307124bd3e95256
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-04-04 21:59:56 +0200

    Refactor getAll function on handlerFactory

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,12 +1,13 @@
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+const APIFeatures = require('./../utils/apiFeatures');
 
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
     const doc = await Model.findByIdAndDelete(req.params.id);
 
     if (!doc) {
-      return next(new AppError('No documet found with that ID', 404));
+      return next(new AppError('No document found with that ID', 404));
     }
 
     res.status(204).json({
@@ -23,7 +24,7 @@ exports.updateOne = (Model) =>
     });
 
     if (!document) {
-      return next(new AppError('No tour found with that ID', 404));
+      return next(new AppError('No document found with that ID', 404));
     }
 
     res.status(200).json({
@@ -43,5 +44,43 @@ exports.createOne = (Model) =>
       data: {
         data: newDocument,
       },
+    });
+  });
+
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions) query = query.populate(popOptions);
+    const doc = await query;
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    // To allow for nested GET reviews on tour (HACK CODE)
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+
+    //EXECUTE THE QUERY
+    const features = new APIFeatures(Model.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const docs = await features.query;
+    res.status(200).json({
+      status: 'success',
+      results: docs.length,
+      data: { data: docs },
     });
   });
--- controllers/userController.js
+++ controllers/userController.js
@@ -10,14 +10,6 @@ const filterObj = (obj, ...allowedFields) => {
   });
   return newObj;
 };
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find();
-  res.status(200).json({
-    status: 'success',
-    results: users.length,
-    data: { users },
-  });
-});
 
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user POSTs password data
@@ -54,19 +46,15 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not defined yet',
-  });
-};
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not defined yet',
+    message: 'This route is not defined yet! Please use sign up instead!',
   });
 };
 
+exports.getAllUsers = factory.getAll(User);
+exports.getUser = factory.getOne(User);
 //DO NOT Update password here
 exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
