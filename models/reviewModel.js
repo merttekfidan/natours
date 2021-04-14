c8d6c853835201ac5334ecafe22947f1beaf1c32
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-04-14 21:42:51 +0200

    Calculate average ratings of reviews on Tours

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -64,18 +64,32 @@ reviewSchema.statics.calcAverageRatings = async function (tourId) {
       },
     },
   ]);
-<<<<<<< HEAD
-=======
-  console.log(stats);
->>>>>>> b44800b7271b4fb6fa1c2f3e3be16730a4c70e41
-  await Tour.findByIdAndUpdate(tourId, {
-    ratingsQuantity: stats[0].nRating,
-    ratingsAverage: stats[0].nRating,
-  });
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
 };
-reviewSchema.post('save', function () {
+
+reviewSchema.post('save', function (next) {
   //This points to current review
   this.constructor.calcAverageRatings(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.findOne();
+  console.log(this.r);
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function (next) {
+  await this.r.constructor.calcAverageRatings(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
