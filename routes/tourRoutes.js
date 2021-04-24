66e5ffb384b62053e0ee200b7e965ebd3885a3fb
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-04-24 03:20:14 +0200

    Implement geospatial aggregation calculating func

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -28,6 +28,8 @@ router.route('/tour-stats').get(tourController.getTourStats);
 router
   .route('/tours-within/:distance/center/:latlng/unit/:unit')
   .get(tourController.getToursWithin);
+
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
 router
   .route('/monthly-plan/:year')
   .get(
