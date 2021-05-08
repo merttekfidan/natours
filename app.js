493e55268b3354820751c52488dafeb477aa3d31
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-05-08 18:21:29 +0200

    Extend base template with blocks

--- app.js
+++ app.js
@@ -79,6 +79,16 @@ app.get('/', (req, res) => {
     user: 'Merdjan',
   });
 });
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+});
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Frest Hiker Tour',
+  });
+});
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
