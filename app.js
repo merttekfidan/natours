11197cd39c0f4c47cc8eba9800c4acd02be965c3
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-09-23 23:52:29 +0200

    Handle webhook-checkout

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controller/bookingController');
 const viewRouter = require('./routes/viewRoutes');
 
 ////////////////////
@@ -51,6 +52,13 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again in an hour!',
 });
 app.use('/api', limiter);
+
+//WE NEED BODY WE NEED RAW. BEFORE CONVERTING TO JSON
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 
 //Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
